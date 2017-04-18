e00fa591fdaad4336961ac0a781edf5618d37afb
rrojas <rrojas@RROJAS>
2017-04-18 16:40:27 -0500

    commit changea

--- Movies/about/components/aboutTextbox.js
+++ Movies/about/components/aboutTextbox.js
@@ -1,5 +1,19 @@
 ﻿export class AboutTextbox {
     constructor() {
-        this.title = "About Textbox";
+        this.title = "About Textbox"; 
+        this.defaultValue = "Roy Rojas";
+    }
+
+    activate() {
+        this.codeHtml = '&lt;my-textbox ' +
+            'obj-id="demo" ' +
+            'name="Demo-Label" ' +
+            'value.bind="defaultValue" ' +
+            'placeholder="Demo"' +
+            '&gt;<br/>' +
+            '&lt;/my-textbox&gt;';
+        this.codeJs = 'constructor() { <br/>' +
+            ' this.defaultValue = "Roy Rojas";<br/>}';
+
     }
 }
