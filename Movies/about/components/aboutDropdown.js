52686737a2bbef05fdba7daf2649c40701195140
rrojas <rrojas@RROJAS>
2017-04-18 15:45:02 -0500

    commit changes

--- Movies/about/components/aboutDropdown.js
+++ Movies/about/components/aboutDropdown.js
@@ -4,16 +4,23 @@
     }
 
     activate() {
-        this.codeHtml = "HTML";
         this.codeJs = "JS";
-        //this.codeHtml = '&lt;my-checkbox ' +
-        //    'obj-id="demo" ' +
-        //    'name="Demo-Label" ' +
-        //    'value.bind="defaultValue"' +
-        //    '&gt;<br/>' +
-        //    '&lt;/my-checkbox&gt;';
-        //this.codeJs = 'constructor() { <br/>' +
-        //    ' this.defaultValue = true;<br/>}';
+        this.codeHtml = '&lt;my-dropdown ' +
+            'obj-id="demo" ' +
+            'name="Demo-Label" ' +
+            'selected.bind="selected" ' +
+            'options.bind="dropdownOptions" ' +
+            '&gt;<br/>' +
+            '&lt;/my-dropdown&gt;';
+        this.codeJs = 'constructor() { <br/>' +
+            ' this.dropdownOptions = [' + '<br/>' +
+            ' \t{ id: "1", text: "Desc 1" },' + '<br/>' +
+            ' \t{ id: "2", text: "Desc 2" },' + '<br/>' +
+            ' \t{ id: "3", text: "Desc 3" },' + '<br/>' +
+            ' \t...' + '<br/>' +
+            ' \t{ id: "n", text: "Desc n" },' + '<br/>' +
+            ' ] ;' + '<br/>' +
+            ' this.selected = this.dropdownOptions[1];<br/>}';
 
         this.dropdownOptions = [
             { id: "1", text: "Desc 1" },
