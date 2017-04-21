1b9170b197d02d6e81824c91a519afacaccde672
rrojas <rrojas@RROJAS>
2017-04-21 11:42:44 -0500

    commit changes

--- Movies/about/components/aboutRadiobuttonList.js
+++ Movies/about/components/aboutRadiobuttonList.js
@@ -1,28 +1,40 @@
 ﻿export class AboutRadiobuttonList {
     constructor() {
         this.title = "About Radiobutton List";
+        this.codeJs = "JS";
+        this.codeHtml = "codeHtml";
     }
 
     activate() {
-        this.codeJs = "JS";
-        this.codeHtml = "codeHtml";
-        //this.codeHtml = '&lt;my-dropdown ' +
-        //    'obj-id="demo" ' +
-        //    'name="Demo-Label" ' +
-        //    'selected.bind="selected" ' +
-        //    'options.bind="dropdownOptions" ' +
-        //    '&gt;<br/>' +
-        //    '&lt;/my-dropdown&gt;';
-        //this.codeJs = 'constructor() { <br/>' +
-        //    ' this.dropdownOptions = [' + '<br/>' +
-        //    ' \t{ id: "1", text: "Desc 1" },' + '<br/>' +
-        //    ' \t{ id: "2", text: "Desc 2" },' + '<br/>' +
-        //    ' \t{ id: "3", text: "Desc 3" },' + '<br/>' +
-        //    ' \t...' + '<br/>' +
-        //    ' \t{ id: "n", text: "Desc n" },' + '<br/>' +
-        //    ' ] ;' + '<br/>' +
-        //    ' this.selected = this.dropdownOptions[1];<br/>}';
+        this.codeHtml = '&lt;my-radiobutton-list ' +
+            'obj-id="demo" ' +
+            'list.bind="radioButtonList" ' +
+            'selected.bind="selectedOption" ' +
+            'name="Demo-RadiobuttonList" ' +
+            'class="radio-primary" ' +
+            'alignment="horizontal" ' +
+            '&gt;<br/>' +
+            '&lt;/my-radiobutton-list&gt;';
+        this.codeJs = 'constructor() { <br/>' +
+            ' this.radioButtonList = [' + '<br/>' +
+            ' \t{ id: "1", text: "Desc 1" },' + '<br/>' +
+            ' \t{ id: "2", text: "Desc 2" },' + '<br/>' +
+            ' \t{ id: "3", text: "Desc 3" },' + '<br/>' +
+            ' \t...' + '<br/>' +
+            ' \t{ id: "n", text: "Desc n" },' + '<br/>' +
+            ' ] ;' + '<br/>' +
+            ' this.selectedOption = this.radioButtonList[1];<br/>}';
 
-      
+        this.radioButtonList = [
+            { id: 1, text: "Desc 1" },
+            { id: 2, text: "Desc 2" },
+            { id: 3, text: "Desc 3" },
+            { id: 4, text: "Desc 4" },
+            { id: 5, text: "Desc 5" }
+        ]
+        this.selectedOption = this.radioButtonList[3];
+        this.selectedPrimaryOption = this.radioButtonList[2];
+        this.selectedHorizontalInfoOption = this.radioButtonList[1];
+        this.selectedHorizontalWarningOption = this.radioButtonList[4];
     }
 }
--- Movies/shared/components/my-radiobutton-list.js
+++ Movies/shared/components/my-radiobutton-list.js
@@ -1,22 +1,13 @@
 ﻿import { bindable, bindingMode } from 'aurelia-framework';
 
-@bindable('id')
+@bindable('objId')
 @bindable('list')
-@bindable('label')
-@bindable('radioname')
+@bindable('name') 
 @bindable({ name: 'selected', defaultBindingMode: bindingMode.twoWay })
 @bindable({ name: 'radioClass', default: '' })
+@bindable({ name: 'alignment', default : ''})
 export class MyRadiobuttonList {
-    constructor() {
-        //this.id = "demo";
-        this.list = [
-            { id: 1, text: "desc 1" },
-            { id: 2, text: "desc 2" },
-            { id: 3, text: "desc 3" },
-            { id: 4, text: "desc 4" },
-            { id: 5, text: "desc 5" },
-        ];
-
-        this.selected = this.list[3];
+    constructor() {  
+        
     }
 }
