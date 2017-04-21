696d81850642cac068b9c8ee2ab894c0b78cc599
rrojas <rrojas@RROJAS>
2017-04-21 15:42:12 -0500

    commit changes

--- Movies/about/components/aboutPicklist.js
+++ Movies/about/components/aboutPicklist.js
@@ -11,5 +11,27 @@
             { id: 3, text: "Desc 3", checked: true },
             { id: 4, text: "Desc 4", checked: false }
         ];
+
+        this.listPrimary = [
+            { id: 1, text: "Desc Primary 1", checked: true },
+            { id: 2, text: "Desc Primary 2", checked: false },
+            { id: 3, text: "Desc Primary 3", checked: true },
+            { id: 4, text: "Desc Primary 4", checked: false }
+        ];
+
+
+        this.listInfo = [
+            { id: 1, text: "Desc Info 1", checked: true },
+            { id: 2, text: "Desc Info 2", checked: false },
+            { id: 3, text: "Desc Info 3", checked: true },
+            { id: 4, text: "Desc Info 4", checked: false }
+        ];
+
+        this.listDanger = [
+            { id: 1, text: "Desc Danger 1", checked: true },
+            { id: 2, text: "Desc Danger 2", checked: false },
+            { id: 3, text: "Desc Danger 3", checked: true },
+            { id: 4, text: "Desc Danger 4", checked: false }
+        ];
     }
 }
